0cbacf88b569cb5ca47e2e1b243d308ef6e589a8
Priti Kumari <pritikashyap18698@gmail.com>
2023-12-22 15:06:47 +0530

    Made Columns

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import './App.css';
+import "./App.css";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+
 import Button from "@mui/material/Button";
+import Displaycard from "./Displaycard";
 
 function App() {
   return (
     <div className="App">
       <div className="navigation">
-        <Button variant="contained">
-          Display
-        </Button>
+        <Button variant="contained">Display</Button>
+        <div className="darkmode">
+          <DarkModeIcon />
+        </div>
       </div>
-      <div className="dashboard">Dashboard</div>
+      <div className="dashboard">
+        <div className="card-container">
+          <div className="inner-container1">
+            Column 1
+            <Displaycard />
+            <Displaycard />
+            <Displaycard />
+          </div>
+          <div className="inner-container2">
+            Column 2
+            <Displaycard />
+          </div>
+          <div className="inner-container3">
+            Column 3
+            <Displaycard />
+            <Displaycard />
+          </div>
+          <div className="inner-container4">
+            Column 4
+            <Displaycard />
+            <Displaycard />
+            <Displaycard />
+          </div>
+          <div className="inner-container5">
+            Column 5
+            <Displaycard />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
